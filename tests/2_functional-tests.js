ab9159ed6c42e6d2da8d659966750b37c52e4c49
Ondrej Klein <ondra.klein@gmail.com>
2022-12-12 14:38:31 +0100

    actually sending some data

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -36,7 +36,7 @@ suite('Functional Tests', function() {
       chai
         .request(server)
         .put('/travellers')
-
+        .send({"surname": "Colombo"})
         .end(function(err, res) {
           console.log(res);
           console.log(res.body);
