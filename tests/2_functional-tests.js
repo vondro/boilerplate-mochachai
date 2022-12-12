7b8b22688dc45c272d4d51fcb8481ba0a760978e
Ondrej Klein <ondra.klein@gmail.com>
2022-12-12 13:39:17 +0100

    fixed comments

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -70,13 +70,13 @@ suite('Functional Tests with Zombie.js', function() {
   });
 
   suite('"Famous Italian Explorers" form', function() {
-    #5
+    // #5
     test('Submit the surname "Colombo" in the HTML form', function(done) {
       assert.fail();
 
       done();
     });
-    #6
+    // #6
     test('Submit the surname "Vespucci" in the HTML form', function(done) {
       assert.fail();
 
